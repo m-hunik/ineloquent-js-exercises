0e0927ef66f968b78f7bd6173051dcb58a149fbc
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-01-03 12:35:10 +0100

    Skip even numbers

---
+++ loops.js
@@ -0,0 +1,13 @@
+// // Skips odd numbers
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         alert(i);
+//     }
+// }
+
+// Skips even numbers, but using continue, without nesting if
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    alert(i);
+}
+
