040487064a6eb8db64fce0eae4d495f15c389619
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-01-04 21:37:07 +0100

    Alert numbers less than 3

--- loops.js
+++ loops.js
@@ -5,9 +5,21 @@
 //     }
 // }
 
-// Skips even numbers, but using continue, without nesting if
-for (let i = 0; i < 10; i++) {
-    if (i % 2 == 0) continue;
-    alert(i);
-}
+// // Skips even numbers, but using continue, without nesting if
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) continue;
+//     alert(i);
+// }
 
+// // Alerts a number up to 2
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+// Same thing as before, but with do... while loop
+let i = 0;
+do {
+    alert(`number ${i}!`);
+    i++;
+} while (i < 3);
+  
