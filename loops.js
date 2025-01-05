568e297620920781657b31230e0c88d61d14eb16
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-01-05 18:09:29 +0100

    Prompt until number > 100

--- loops.js
+++ loops.js
@@ -1,25 +1,38 @@
-// // Skips odd numbers
+////////// Skips odd numbers
+
 // for (let i = 0; i < 10; i++) {
 //     if (i % 2 == 0) {
 //         alert(i);
 //     }
 // }
 
-// // Skips even numbers, but using continue, without nesting if
+////////// Skips even numbers, but using continue, without nesting if
+
 // for (let i = 0; i < 10; i++) {
 //     if (i % 2 == 0) continue;
 //     alert(i);
 // }
 
-// // Alerts a number up to 2
+////////// Alerts a number up to 2
+
 // for (let i = 0; i < 3; i++) {
 //     alert( `number ${i}!` );
 //   }
 
-// Same thing as before, but with do... while loop
-let i = 0;
+////////// Same thing as before, but with do... while loop
+
+// let i = 0;
+// do {
+//     alert(`number ${i}!`);
+//     i++;
+// } while (i < 3);
+
+////////// Prompts for number until it's higher than 100
+
+let numberInput;
+
 do {
-    alert(`number ${i}!`);
-    i++;
-} while (i < 3);
+    numberInput = prompt("Yo, enter number > 100");
+} while (numberInput <= 100);
+
   
