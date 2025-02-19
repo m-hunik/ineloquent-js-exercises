d3d78522dae0707a0496b049aa85d54df95db598
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-02-19 19:17:40 +0100

    Return true if there are no props, false otherwise

--- objects.js
+++ objects.js
@@ -8,5 +8,23 @@ delete user.name;
 console.log(user.name);
 
 let emptyTestObject = { age: 36, money: null };
+emptyTestObject["health status"] = "mysterious infection in progress";
 
-console.log("age" in emptyTestObject, "money" in emptyTestObject);
+console.log(
+  "age" in emptyTestObject,
+  "money" in emptyTestObject,
+  "health status" in emptyTestObject
+);
+
+for (let key in emptyTestObject) {
+  console.log(key); // prints keys
+  console.log(emptyTestObject[key]); // prints values for the keys
+}
+
+function isEmpty(emptyTestObject) {
+  for (let key in emptyTestObject) {
+    // iterate through keys
+    return false; // returns false if there are any properties (loops starts)
+  }
+  return true; // return true if there's no properties
+}
