8b0f9682006bdace029227b864ec386837539d9e
michal <139953019+m-hunik@users.noreply.github.com>
2025-07-28 20:57:54 +0200

    Refresh knowledge about objects

--- objects.js
+++ objects.js
@@ -3,45 +3,4 @@ let user = {
   surname: "Smith",
 };
 
-user.name = "Pete";
-delete user.name;
 console.log(user.name);
-
-let emptyTestObject = { age: 36, money: null };
-emptyTestObject["health status"] = "mysterious infection in progress";
-
-console.log(
-  "age" in emptyTestObject,
-  "money" in emptyTestObject,
-  "health status" in emptyTestObject
-);
-
-for (let key in emptyTestObject) {
-  console.log(key); // prints keys
-  console.log(emptyTestObject[key]); // prints values for the keys
-}
-
-function isEmpty(emptyTestObject) {
-  for (let key in emptyTestObject) {
-    // iterate through keys
-    return false; // returns false if there are any properties (loops starts)
-  }
-  return true; // return true if there's no properties
-}
-
-let menu = {
-  width: 200,
-  height: 400,
-  title: "elooo",
-};
-
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] === "number") {
-      obj[key] *= 2;
-      console.log(obj[key]);
-    }
-  }
-}
-
-console.log(multiplyNumeric(menu));
