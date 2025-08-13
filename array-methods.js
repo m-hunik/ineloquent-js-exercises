51a03c9b0e9583744e76c87e44c716d02c47f415
michal <139953019+m-hunik@users.noreply.github.com>
2025-08-13 22:37:31 +0200

    Map users array to new objects with full name

--- array-methods.js
+++ array-methods.js
@@ -1,8 +1,26 @@
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// Map users to names
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [john, pete, mary];
+// let names = users.map(item => item.name);
+
+// console.log(names);
+
+// Map to objects
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let users = [john, pete, mary];
-let names = users.map(item => item.name);
 
-console.log(names);
+let usersMapped = users.map((person) => ({
+  fullname: `${person.name}, ${person.surname}`,
+  id: person.id,
+}));
+
+console.log(usersMapped[1].id);
+console.log(usersMapped[1].fullname);
